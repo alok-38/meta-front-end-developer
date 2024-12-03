c27504d442e24e7b0ec99339bec4857f4c9129a6
Alokananda Y <plantsfortrees@gmail.com>
2024-12-03 02:27:58 +0000

    Object Oriented Programming principles

---
+++ react-basics/js-modules/named-exports/index.js
@@ -0,0 +1,9 @@
+function addTwo(a, b) {
+    return a + b;  // Return the sum, not log it
+}
+
+function addThree(a, b, c) {
+    return a + b + c;  // Return the sum, not log it
+}
+
+export { addTwo, addThree };
---
+++ react-basics/js-modules/named-exports/mathOperations.js
@@ -0,0 +1,3 @@
+import { addTwo, addThree } from "./index.js";
+
+console.log(addThree(3, 3, 3) * addTwo(2, 2));  // This should now work correctly
